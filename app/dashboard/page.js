7b6c3afc4ba66f49133ad81fb937abcb5903d224
McLainCronin <cronin97@gmail.com>
2024-11-03 20:36:20 -0800

    Small update

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "Moodl ⋅ Dashboard",
+}
+
 export default function DashboardPage() {
     return (
         <div></div>
--- app/layout.js
+++ app/layout.js
@@ -20,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header></header>
         {children}
+        <footer></footer>
       </body>
     </html>
   );

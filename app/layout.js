0199d096c53673422ba5260aa4e00337cbdae00d
McLainCronin <cronin97@gmail.com>
2024-11-08 20:28:39 -0800

    Add link to homepage in header title on RootLayout

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   const header = (
     <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
-      <h1 className={'text-based sm:text-lg textGradient ' + fugaz.className}>Moodi</h1>
+      <h1 className={'text-based sm:text-lg textGradient ' + fugaz.className}><a href='/'>Moodi</a></h1>
       <div className="flex items-center justify-between">
         PLACEHOLDER CTA || STATS
       </div>

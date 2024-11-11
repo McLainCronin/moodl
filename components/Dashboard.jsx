8d1200d1c4dea7a9935661fdb15e0f369424f441
McLainCronin <cronin97@gmail.com>
2024-11-10 19:47:32 -0800

    Add Calendar component to Dashboard and enhance styling; introduce purpleShadow class for button effects

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Main from './Main'
 import { Fugaz_One } from "next/font/google";
+import Calendar from './Calendar';
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
@@ -20,12 +21,12 @@ export default function Dashboard() {
   }
 
   return (
-    <div className='flex flex-col flex-1 gap-4 sm:gap-8 md:gap-12'>
-        <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg'>
+    <div className='flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16'>
+        <div className='grid grid-cols-3 bg-indigo-50 text-indigo-500 p-4 gap-4 rounded-lg'>
           {Object.keys(statuses).map((status, statusIndex) => {
             return (
               <div key={statusIndex} className='p-4 flex flex-col gap-1 sm:gap-2'>
-                <p className='font-medium uppercase text-xs sm:text-sm'>{status.replaceAll('_', ' ')}</p>
+                <p className='font-medium uppercase text-xs sm:text-sm truncate'>{status.replaceAll('_', ' ')}</p>
                 <p className={'text-base sm:text-lg ' + fugaz.className}>{statuses[status]}</p>
               </div>
             )
@@ -34,16 +35,17 @@ export default function Dashboard() {
         <h4 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}>
           How do you <span className='textGradient'>Feel</span> today?
         </h4>
-        <div className='grid grid-cols-2 md:grid-cols-5 gap-4'>
+        <div className='flex items-stretch flex-wrap gap-4'>
           {Object.keys(moods).map((mood, moodIndex) => {
             return (
-              <button className={' ' + (moodIndex === 4 ? ' col-span-2 md:col-span-1' : ' ' )} key={moodIndex}>
-                <p>{mood}</p>
-                <p>{moods[mood]}</p>
+              <button className={'p-4 px-6 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-[lavender] text-center flex flex-col items-center gap-2 flex-1 '} key={moodIndex}>
+                <p className='text-4xl sm:text-5xl md:text-6xl'>{moods[mood]}</p>
+                <p className={'text-indigo-500 text-xs sm:text-sm md:text-base ' + fugaz.className}>{mood}</p>
               </button>
             )
           })}
         </div>
+        <Calendar />
     </div>
   )
 }

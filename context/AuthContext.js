11530d5c08f7b53d43d04d46e5edac7e8ef333c2
McLainCronin <cronin97@gmail.com>
2024-11-10 21:07:32 -0800

    Enhance AuthContext with user logout functionality and Firestore data fetching on auth state change

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,9 @@
 'use client'
 import React, { useEffect, useContext, useState } from 'react'
 import { auth } from '@/lib/firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { db } from '@/lib/firebase'
+import { doc, getDoc } from 'firebase/firestore'
 
 
 
@@ -25,8 +28,48 @@ export function AuthProvider({ children }) {
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const value = {
+    function logout() {
+        setUserDataObj({})
+        setCurrentUser(null)
+        return signOut(auth)
+    }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async user => {
+            try {
+                //Set the user to our local context state
+                setLoading(true)
+                setCurrentUser(user)
+                if (!user) {
+                    return
+                }
+
+                // if user exists, fetch data from firestore database
+                console.log('Fetching data from firestore...')
+                const docRef = doc(db, 'users', user.uid)
+                const docSnap = await getDoc(docRef)
+                let firebaseData = {}
+                if (docSnap.exists()) {
+                    console.log('Found data')
+                    firebaseData = docSnap.data()
+                }
+                setUserDataObj(firebaseData)
+            } catch (err) {
+                console.log(err.message)
+            } finally {
+                setLoading(false)
+            }
+        })
+        return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser,
+        userDataObj,
+        signup,
+        logout,
+        login,
+        loading
     }
     
     return (

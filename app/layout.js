431defd8bd9d980df13d1eb13f1b195bc687f21d
McLainCronin <cronin97@gmail.com>
2024-11-11 16:47:25 -0800

    Add Logout component and integrate it into the layout; conditionally render based on user authentication status

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/AuthContext";
 import Head from "./head";
+import Logout from "@/components/Logout";
 
 
 const opensans = Open_Sans({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           Moodi
         </Link>
       </h1>
+      <Logout />
     </header>
   )
 
